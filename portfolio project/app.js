5ac9d5548566e5e8df73f9a07109041aa2056d40
rkeerthick <rkeerthick1@gmail.com>
2023-10-03 15:02:40 +0530

    3/10/23 second commit

--- portfolio project/app.js
+++ portfolio project/app.js
@@ -2,12 +2,22 @@ let tabLinks = document.getElementsByClassName("tab-links");
 let tabContents = document.getElementsByClassName("tab-contents");
 
 const openTab = (tabName) => {
-    for(tabLink of tabLinks) {
-        tabLink.classList.remove("active-link");
-    }
-    for(tabContent of tabContents) {
-        tabContent.classList.remove("active-tab");
-    }
-    event.target.classList.add("active-link");
-    document.getElementById(tabName).classList.add("active-tab");
-}
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.target.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+};
+
+let sidebar = document.getElementById("sidebar");
+
+const openMenu = () => {
+    sidebar.style.right = "0";
+};
+
+const closeMenu = () => {
+    sidebar.style.right = "-200px";
+};
